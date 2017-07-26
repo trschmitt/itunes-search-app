42f6b55c15ae1b38d177d2f4502bff2639fb4a1c
Tristan Schmitt <trschmitt50@live.com>
2017-07-26 19:46:39 -0400

    Search is working, audio player is currently not hooked up that is the next task

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 
 let list = document.querySelector("#list");
 let baseURL = "https://itunes.apple.com/search"
-let search = document.querySelector(".form");
+let search = document.querySelector("form");
 
 
 search.addEventListener("submit", (event) => {
@@ -10,23 +10,23 @@ search.addEventListener("submit", (event) => {
   let searchTerm = event.target.querySelector("input").value;
   let userReq = `${baseURL}?term=${searchTerm}`
 
-  fetch(userReq)
-  .then(
-    (response) => {
+  fetch(userReq).then((response) => {
       if (response.status !== 200) {
         console.log("The status is " + response.status);
       }
 
       response.json().then((musicData) => {
+        console.log(musicData);
         let result = musicData.results;
         let templateContainer = "";
         let template = "";
-        results.forEach((song) => {
+        result.forEach((song) => {
+          console.log(song);
           template = `
             <li>
-              <img src="${artworkUrl100}" alt="recipe-picture-not-found">
-              <a href="${collectionViewUrl}"><h4>${artistName}</h4></a>
-              <a href="${trackViewUrl}"><h5>${trackName}</h5></a>
+              <img src="${song.artworkUrl100}" alt="picture-not-found">
+              <a href="${song.collectionViewUrl}"><h4>${song.artistName}</h4></a>
+              <a href="${song.trackViewUrl}"><h5>${song.trackName}</h5></a>
             </li>
           `
           templateContainer += template;
