5e7734de78a29ea6c8a832d1538c2757b936fca1
Tristan Schmitt <trschmitt50@live.com>
2017-08-19 14:29:44 -0400

    Continued to style and now it is up to standards

--- main.js
+++ main.js
@@ -1,40 +1,49 @@
+let list    = document.querySelector("#list");
+let baseURL = "https://itunes.apple.com/search";
+let search  = document.querySelector("form");
+let music   = document.querySelector("img");
 
-let list = document.querySelector("#list");
-let baseURL = "https://itunes.apple.com/search"
-let search = document.querySelector("form");
-
-
-search.addEventListener("submit", (event) => {
+search.addEventListener("submit", event => {
   event.preventDefault();
 
   let searchTerm = event.target.querySelector("input").value;
-  let userReq = `${baseURL}?term=${searchTerm}`
+  let userReq    = `${baseURL}?term=${searchTerm}`;
 
-  fetch(userReq).then((response) => {
-      if (response.status !== 200) {
-        console.log("The status is " + response.status);
-      }
+  fetch(userReq).then(response => {
+    if (response.status !== 200) {
+      console.log("The status is " + response.status);
+    }
 
-      response.json().then((musicData) => {
-        console.log(musicData);
-        let result = musicData.results;
-        let templateContainer = "";
-        let template = "";
-        result.forEach((song) => {
-          console.log(song);
-          template = `
+    response.json().then(musicData => {
+      console.log(musicData);
+      let result   = musicData.results;
+      let templateContainer = "";
+      let template = "";
+      result.forEach(song => {
+        template = `
             <li>
-              <img src="${song.artworkUrl100}" alt="picture-not-found">
+              <a href="${song.previewUrl}"><img class="song-preview" src="${song.artworkUrl100}" alt="picture-not-found"></a>
               <a href="${song.collectionViewUrl}"><h4>${song.artistName}</h4></a>
               <a href="${song.trackViewUrl}"><h5>${song.trackName}</h5></a>
             </li>
-          `
-          templateContainer += template;
+          `;
+        templateContainer += template;
+      });
+      searchTerm = "";
+      list.innerHTML = templateContainer;
+
+      let previewSong = document.getElementsByClassName("song-preview");
+      let musicPlayer = document.querySelector("audio");
+
+      for (let i = 0; i < previewSong.length; i++) {
+        previewSong[i].addEventListener("click", (e) => {
+          e.preventDefault();
+          musicPlayer.setAttribute("src", result[i].previewUrl);
         });
-        searchTerm = "";
-        list.innerHTML = templateContainer;
-      })
-    }
-  )
-})
+      };
+
+    });
+  });
+});
+
 // 5. Create a way to listen for a click that will play the song in the audio play
